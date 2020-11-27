7e1b05155d068971295fd640f1bc14ccf3f3d616
Jonathan Hamberg <jonathanhamberg@gmail.com>
2020-11-27 14:23:48 -0800

    Initial commit.

---
+++ cmake/git_version.h
@@ -0,0 +1,16 @@
+/*       _                       __        ___          _
+ *      | | ___  _____   ____ _  \ \      / (_)_ __ ___| | ___  ___ ___
+ *   _  | |/ _ \/ _ \ \ / / _` |  \ \ /\ / /| | '__/ _ \ |/ _ \/ __/ __|
+ *  | |_| |  __/  __/\ V / (_| |   \ V  V / | | | |  __/ |  __/\__ \__ \
+ *   \___/ \___|\___| \_/ \__,_|    \_/\_/  |_|_|  \___|_|\___||___/___/
+ *
+ * Author: Jonathan Hamberg (jonathan.hamberg@jeevawireless.com)
+ * Copyright: 2020 Jeeva Wireless
+ * License: Proprietary
+ */
+#ifndef HELLO_GIT_VERSION_H
+#define HELLO_GIT_VERSION_H
+
+extern const char *kGitHash;
+
+#endif // HELLO_GIT_VERSION_H
---
+++ embedded-git-hash/main.cpp
@@ -0,0 +1,8 @@
+#include <iosteam>
+#include "git_vesion.h"
+
+int main()
+{
+    std::cout << "Git Hash: " << kGitHash << "\n";
+    return 0;
+}
